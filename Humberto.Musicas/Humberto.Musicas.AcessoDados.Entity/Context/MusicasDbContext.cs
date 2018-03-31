855bf3d614c35bb1760abb9043d78793418e07f7
humberto Alves <humbertoalvesmoraes@gmail.com>
2018-03-30 22:27:59 -0300

    primeiro commit again

---
+++ Humberto.Musicas/Humberto.Musicas.AcessoDados.Entity/Context/MusicasDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Humberto.Musicas.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humberto.Musicas.AcessoDados.Entity.Context
+{
+    public class MusicasDbContext : DbContext
+    {
+        public DbSet<Album> Albuns { get; set; }
+    }
+}
